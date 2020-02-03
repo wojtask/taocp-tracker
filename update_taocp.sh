bc93efd07a967a2b77f8c528ac09a12247a852f2
Krzysztof Wojtas <kwojtas@gmail.com>
2020-02-03 13:43:33 +0100

    New fasc8b, prettier script output, cleanup

--- update_taocp.sh
+++ update_taocp.sh
@@ -5,18 +5,25 @@ TAOCPDIR=.
 DIFFPDF=diff-pdf/diff-pdf
 NEW_UPDATES=
 
-for code in `seq 7 10`; do
+for code in {7..10}; do
     for letter in a b c; do
         FASC=fasc${code}${letter}.ps.gz
         OLDMD5=`sha1sum $TAOCPDIR/$FASC 2>/dev/null | cut -d' ' -f1`
         wget -q $URL/$FASC -O $TAOCPDIR/$FASC
+
         if [ ! -s $TAOCPDIR/$FASC ]; then
             rm $TAOCPDIR/$FASC
         else
             NEWMD5=`sha1sum $TAOCPDIR/$FASC 2>/dev/null | cut -d' ' -f1`
             echo -n "Checking ${FASC%.ps.gz}... "
+
+            if [ ! $OLDMD5 ]; then
+                echo -en '**\e[5;32mNEW!\e[0m** (Please update the report manually)'
+            elif [ $OLDMD5 != $NEWMD5 ]; then
+                echo -en '\e[5;33mUPDATED!\e[0m'
+            fi
+
             if [ ! $OLDMD5 ] || [ $OLDMD5 != $NEWMD5 ]; then
-                echo -n "UPDATED!"
                 FASCPDF=${FASC%ps.gz}pdf
                 if [ -f $TAOCPDIR/$FASCPDF ]; then
                     mv $TAOCPDIR/$FASCPDF $TAOCPDIR/${FASCPDF%.pdf}_old.pdf
@@ -26,19 +33,11 @@ for code in `seq 7 10`; do
                 PAGE_COUNT=`pdfinfo $TAOCPDIR/$FASCPDF | grep Pages | tr -s " " | cut -d " " -f 2`
 
                 sed -i "/$FASCPDF/{p;N;s:<td>.*</td>:<td>$PAGE_COUNT</td>:}" $TAOCPDIR/taocp.html
-
-                #$DIFFPDF --output-diff=${FASCPDF%.pdf}_diff.pdf $TAOCPDIR/${FASCPDF%.pdf}_old.pdf $TAOCPDIR/$FASCPDF
-                NEW_UPDATES="$NEW_UPDATES ${FASC%.ps.gz}"
             fi
             echo
         fi
     done
 done
-
-if [ "$NEW_UPDATES" ]; then
-    echo UPDATED $NEW_UPDATES !!!
-    #notifu-1.6/notifu64 /t info /p "New updates for TAoCP!" /m "The following fascicles got updated:$NEW_UPDATES"
-fi
 
 CURRENT_DATE=`date +%Y-%m-%d`
 sed -i "/Revision date/s:....-..-..:$CURRENT_DATE:" $TAOCPDIR/taocp.html
